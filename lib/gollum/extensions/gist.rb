cdc37d26f1d612a8d56ab8fb4a400525824ea445
Jon Frisby <jfrisby@mrjoy.com>
2010-10-12 21:10:11 -0700

    Remove spurious comments. *cough*

--- lib/gollum/extensions/gist.rb
+++ lib/gollum/extensions/gist.rb
@@ -1,14 +1,5 @@
-# This is going to serve as an example class, showing of how to use
-# the extension API in the simplest way possible.
-#   We start of by creating the class. It doesn't actually have to be
-# a class in Gollum.
 module Gollum
-  # Neither is it neccessary to extend ExtensionTag, but this class 
-  # has the initialize method already.
-  #   If we wanted to, we could create our own initialize, as long as
-  # we have the two arguments
   class GistLink < Gollum::ExtensionTag
-    # The 'render' method is needed
     def render
       args = @arguments.split('|')
       gist = args.first
@@ -18,8 +9,4 @@ module Gollum
   end
 end
 
-# Finally, when the class has been made, we have to register it.
-#   A class is registrated by calling the following function like so
 Gollum::ExtensionTag.register_extension_tag('gist', Gollum::GistLink)
-# The first argument is the ´tag´ Gollum will be looking for, the 
-# second is the class that is going to do the magic.
